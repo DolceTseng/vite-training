9826a79674aeed90e3d72598664c6ee88892ef35
DolceTseng <jessica.turn613@gmail.com>
2024-07-26 13:27:06 +0800

    update

--- main.js
+++ main.js
@@ -1,3 +1,9 @@
 import './assets/scss/all.scss';
 
 console.log("Hello world!");
+
+$(window).scroll(function() {
+    if ($(this).scrollTop() > 100) {
+      console.log("Scroll to Position");
+    }
+  });
